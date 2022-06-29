a60d888907b395b20d4c0965827c897b54d262db
chinonso ata <chinonso.ata@gmail.com>
2022-06-29 23:10:23 +0100

    Displayed Text to test that the application was working

--- src/main/java/com/nony/fleetmsv2/ApplicationController.java
+++ src/main/java/com/nony/fleetmsv2/ApplicationController.java
@@ -1,5 +1,13 @@
 package com.nony.fleetmsv2;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ApplicationController {
 
+	@GetMapping("/index")
+	public String home() {
+		return "index";
+	}
 }

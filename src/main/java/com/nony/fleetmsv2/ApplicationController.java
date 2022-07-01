8f8e001a39e44a86ea03c636ef654e16ba13c879
chinonso ata <chinonso.ata@gmail.com>
2022-07-02 00:53:25 +0100

    Created the routes for the 6 modules

--- src/main/java/com/nony/fleetmsv2/ApplicationController.java
+++ src/main/java/com/nony/fleetmsv2/ApplicationController.java
@@ -11,8 +11,33 @@ public class ApplicationController {
 		return "index";
 	}
 
-	@GetMapping("/index2")
-	public String layout() {
-		return "index2";
+	@GetMapping("/hr")
+	public String hr() {
+		return "/hr/index";
+	}
+
+	@GetMapping("/fleet")
+	public String fleet() {
+		return "/vehicles/index";
+	}
+
+	@GetMapping("/helpdesk")
+	public String helpdesk() {
+		return "/helpdesk/index";
+	}
+
+	@GetMapping("/accounts")
+	public String accounts() {
+		return "/accounts/index";
+	}
+
+	@GetMapping("/payroll")
+	public String payroll() {
+		return "/payroll/index";
+	}
+
+	@GetMapping("/parameters")
+	public String parameters() {
+		return "/parameters/index";
 	}
 }

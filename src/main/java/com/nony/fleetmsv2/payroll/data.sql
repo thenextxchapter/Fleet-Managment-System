f24ba271180accaea06de17fabfd040b600c3807
chinonso ata <chinonso.ata@gmail.com>
2022-07-02 10:53:22 +0100

    Script for Inserting initial data into the database

---
+++ src/main/java/com/nony/fleetmsv2/payroll/data.sql
@@ -0,0 +1,31 @@
+INSERT INTO country VALUE (
+                           1,
+                           'Washington DC',
+                           'USA',
+                           'North America',
+                           'United States',
+                           'American');
+
+INSERT INTO country VALUE (
+                           2,
+                           'Abuja',
+                           'NGN',
+                           'Africa',
+                           'Nigeria',
+                           'Nigerian');
+
+INSERT INTO country VALUE (
+                           3,
+                           'New Delhi',
+                           'IN',
+                           'Asia',
+                           'India',
+                           'Indian');
+
+INSERT INTO country VALUE (
+                           4,
+                           'Budapest',
+                           'HUN',
+                           'Europe',
+                           'Hungary',
+                           'Hungarian');

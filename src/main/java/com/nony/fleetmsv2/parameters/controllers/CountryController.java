058c0a363fea923d37e1bb99e6545231726e84b0
chinonso ata <chinonso.ata@gmail.com>
2022-07-02 19:06:10 +0100

    Added the Model parameter to the getAll method

--- src/main/java/com/nony/fleetmsv2/parameters/controllers/CountryController.java
+++ src/main/java/com/nony/fleetmsv2/parameters/controllers/CountryController.java
@@ -6,6 +6,7 @@ import com.nony.fleetmsv2.parameters.models.Country;
 import com.nony.fleetmsv2.parameters.services.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,9 +17,9 @@ public class CountryController {
 	private CountryService countryService;
 
 	@GetMapping("/countries")
-	public String getAll() {
+	public String getAll(Model model) {
 		List<Country> countries = countryService.getAll();
-
+		model.addAttribute("countries", countries);
 		return "parameters/countryList";
 	}
 }

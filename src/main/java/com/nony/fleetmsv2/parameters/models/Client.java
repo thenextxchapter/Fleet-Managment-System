e1052a1c1b521141353234833b21e994cc183ede
chinonso ata <chinonso.ata@gmail.com>
2022-07-02 08:27:57 +0100

    Created all the necessary fields in the class

--- src/main/java/com/nony/fleetmsv2/parameters/models/Client.java
+++ src/main/java/com/nony/fleetmsv2/parameters/models/Client.java
@@ -1,6 +1,34 @@
 package com.nony.fleetmsv2.parameters.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class)
 public class Client {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	private String name;
+	private String address;
+	private String city;
+	private String phone;
+	private String mobile;
+	private String website;
+	private String email;
 }

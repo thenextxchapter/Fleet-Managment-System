19334aac2217ed817a82dd5dbd304e7c6dfb40f2
chinonso ata <chinonso.ata@gmail.com>
2022-07-02 08:44:48 +0100

    Created the Module entity model

--- src/main/java/com/nony/fleetmsv2/parameters/models/Module.java
+++ src/main/java/com/nony/fleetmsv2/parameters/models/Module.java
@@ -1,5 +1,17 @@
 package com.nony.fleetmsv2.parameters.models;
 
-public class Module {
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+
+public class Module extends CommonObject {
 
 }

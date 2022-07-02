8697235c9422e30bdb9fcfb2c5cd456c2ea484f1
chinonso ata <chinonso.ata@gmail.com>
2022-07-02 09:00:19 +0100

    replaced the @Data annotation with hash methods

--- src/main/java/com/nony/fleetmsv2/parameters/models/Module.java
+++ src/main/java/com/nony/fleetmsv2/parameters/models/Module.java
@@ -1,17 +1,37 @@
 package com.nony.fleetmsv2.parameters.models;
 
 import javax.persistence.Entity;
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 @Entity
-@Data
-@EqualsAndHashCode(callSuper=false)
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 
 public class Module extends CommonObject {
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+			return false;
+		Module module = (Module) o;
+		return getId() != null && Objects.equals(getId(), module.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
 }

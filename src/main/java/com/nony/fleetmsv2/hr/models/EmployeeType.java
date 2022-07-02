fceb15ff5f3dfc7a439143959ea6fcb6f46e9c7b
chinonso ata <chinonso.ata@gmail.com>
2022-07-02 09:06:47 +0100

    Created the hr EmployeeType entity model

--- src/main/java/com/nony/fleetmsv2/hr/models/EmployeeType.java
+++ src/main/java/com/nony/fleetmsv2/hr/models/EmployeeType.java
@@ -1,5 +1,17 @@
 package com.nony.fleetmsv2.hr.models;
 
-public class EmployeeType {
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.nony.fleetmsv2.parameters.models.CommonObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class EmployeeType extends CommonObject {
 
 }

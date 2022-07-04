152fab1ef24c0515352df61cd33b136b765fd2c8
chinonso ata <chinonso.ata@gmail.com>
2022-07-04 11:31:24 +0100

    Created the delete method in the controller

--- src/main/java/com/nony/fleetmsv2/parameters/controllers/StateController.java
+++ src/main/java/com/nony/fleetmsv2/parameters/controllers/StateController.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class StateController {
@@ -31,6 +34,18 @@ public class StateController {
 	@PostMapping("/states")
 	public String save(State state) {
 		stateService.save(state);
+		return "redirect:/states";
+	}
+
+	@RequestMapping(
+			value = "states/delete/{id}",
+			method = {
+					RequestMethod.GET,
+					RequestMethod.DELETE
+			}
+	)
+	public String delete(@PathVariable Integer id) {
+		stateService.delete(id);
 		return "redirect:/states";
 	}
 }

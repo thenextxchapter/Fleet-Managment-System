86504eda77a27c0ef7e37b1967a07f776dbcdd72
chinonso ata <chinonso.ata@gmail.com>
2022-07-04 10:04:21 +0100

    Create a get all states method in the controller

---
+++ src/main/java/com/nony/fleetmsv2/parameters/controllers/StateController.java
@@ -0,0 +1,24 @@
+package com.nony.fleetmsv2.parameters.controllers;
+
+import java.util.List;
+
+import com.nony.fleetmsv2.parameters.models.State;
+import com.nony.fleetmsv2.parameters.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StateController {
+
+	@Autowired
+	private StateService stateService;
+
+	@GetMapping("/states")
+	private String getAll(Model model) {
+		List<State> states = stateService.getAll();
+		model.addAttribute("states", states);
+		return "parameters/stateList";
+	}
+}

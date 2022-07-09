0d32c849a471d0b8f466752e7f05cf53572ef15c
chinonso ata <chinonso.ata@gmail.com>
2022-07-09 11:45:03 +0100

    Created the Contact repository

---
+++ src/main/java/com/nony/fleetmsv2/parameters/repositories/ContactRepository.java
@@ -0,0 +1,10 @@
+package com.nony.fleetmsv2.parameters.repositories;
+
+import com.nony.fleetmsv2.parameters.models.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+
+}

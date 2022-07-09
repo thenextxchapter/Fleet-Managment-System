1620eae1c5f8bb3b3adf5d2110ac3e5f1c678649
chinonso ata <chinonso.ata@gmail.com>
2022-07-09 11:44:12 +0100

    Created the routes for the contact module

--- src/main/java/com/nony/fleetmsv2/parameters/controllers/ContactController.java
+++ src/main/java/com/nony/fleetmsv2/parameters/controllers/ContactController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import com.nony.fleetmsv2.parameters.models.Contact;
+import com.nony.fleetmsv2.parameters.models.Location;
 import com.nony.fleetmsv2.parameters.services.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,30 +26,53 @@ public class ContactController {
 	public String getAll(Model model){
 		List<Contact> contacts =   contactService.getAll();
 		model.addAttribute("contacts", contacts);
-		return "/parameters/contact/contacts";
+		return "parameters/contact/contactList";
+	}
+
+	@GetMapping("/parameters/contact/{id}")
+	@ResponseBody
+	public Contact getContact(@PathVariable Integer id) {
+		return contactService.getById(id);
 	}
 
 	@GetMapping("/parameters/add-contact")
-	public String contactAdd() { return "/parameters/contact/contacts"; }
+	public String addContact() {
+		return "parameters/contact/contactAdd";
+	}
 
-	@RequestMapping("contacts/findById")
-	@ResponseBody
-	public Contact getById(Integer id) { return contactService.getById(id); }
-
-	@PostMapping(value = "contacts/addNew")
-	public String addNew(Contact contact){
+	@PostMapping("/parameters/contacts")
+	public String save(Contact contact){
 		contactService.save(contact);
 		return "redirect:/parameters/contacts";
 	}
 
+	@GetMapping("/parameters/edit-contact/{id}")
+	public String editContact(@PathVariable Integer id, Model model){
+		Contact contact = contactService.getById(id);
+		model.addAttribute("contact", contact);
+		return "parameters/contact/contactEdit";
+	}
+
 	@RequestMapping(
-			value = "contacts/delete",
+			value = "/parameters/contacts/delete/{id}",
 			method = {
 					RequestMethod.DELETE,
 					RequestMethod.GET
 			})
-	public String delete(Integer id) {
+	public String delete(@PathVariable Integer id) {
 		contactService.delete(id);
+		return "redirect:/parameters/contacts";
+	}
+
+	@RequestMapping(
+			value = "/parameters/contacts/update/{id}",
+			method = {
+					RequestMethod.GET,
+					RequestMethod.PUT
+			}
+	)
+	public String update(Contact contact) {
+		contactService.save(contact);
 		return "redirect:/parameters/contacts";
 	}
 }
